378dfab93f455dbb725a4aa09b13cd5a19ed286f
Moritz Reiter <moritz@anothernode.com>
2017-11-11 14:23:35 +0100

    Add time format for fish themes

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,6 +1,8 @@
 set -x EDITOR vim
 set -x PYTHONSTARTUP ~/.pythonrc
 
+set -g theme_date_format "+%a %Y-%m-%d %H:%M:%S"
+
 # Load OS specific stuff
 set workDir (dirname (status -f))
 switch (uname)
