907ad0da1a2be239636fe639941986960c3b4c8b
Moritz Reiter <moritz@anothernode.com>
2017-11-12 19:05:13 +0100

    Add ssh-agent/keychain setup to Linux fish config

--- home/.config/fish/config.linux.fish
+++ home/.config/fish/config.linux.fish
@@ -1,1 +1,14 @@
 # Linux specific fish configuration here
+
+# Set up keychain, which persists SSH keys in SSH agent across login sessions
+# https://www.funtoo.org/Keychain
+if begin type -q keychain; and status --is-interactive; end
+  keychain --eval --quiet --quick --nogui --noask id_rsa | source
+end
+
+set no_identities "The agent has no identities."
+if begin type -q ssh-add; and [ (ssh-add -l) = $no_identities ]; end
+  echo "SSH agent reported to have no identities stored."
+  echo "Let me run `ssh-add` for you..."
+  ssh-add
+end
