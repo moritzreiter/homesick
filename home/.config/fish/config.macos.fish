f24e51df07ba60ac1e5de22bca1523e915f4e428
Moritz Reiter <moritz@anothernode.com>
2017-12-07 22:22:40 +0100

    Improve fish Java environment configuration
    Now, environment variables for different Java versions only get set if
    the respective version is actually installed. This prevents error
    messages when logging in to a fish session.

--- home/.config/fish/config.macos.fish
+++ home/.config/fish/config.macos.fish
@@ -1,6 +1,13 @@
 # Java
-set -x JAVA_8_HOME (/usr/libexec/java_home -v1.8)
-set -x JAVA_9_HOME (/usr/libexec/java_home -v9)
+set -x JAVA_8_HOME ''
+set -x JAVA_9_HOME ''
+set java_home_cmd '/usr/libexec/java_home 2>/dev/null --failfast'
+if test (eval $java_home_cmd -v1.8)
+    set -x JAVA_8_HOME (eval $java_home_cmd -v1.8)
+end
+if test (eval $java_home_cmd -v9)
+    set -x JAVA_9_HOME (eval $java_home_cmd -v9)
+end
 set -x JAVA_HOME $JAVA_8_HOME
 
 alias java8='set -x JAVA_HOME $JAVA_8_HOME'
