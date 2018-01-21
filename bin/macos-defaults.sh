515bf8c1f2d4eded1e0e1f739cd6f5e187d0f795
Moritz Reiter <moritz@anothernode.com>
2018-01-21 16:42:07 +0100

    Disable `ApplePressAndHold` for IDEs

--- bin/macos-defaults.sh
+++ bin/macos-defaults.sh
@@ -1,2 +1,9 @@
+#!/bin/sh
+
 # Display full POSIX path as Finder window title
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# Disable `ApplePressAndHold` in IDEs to avoid conflicts with vim mode
+defaults write com.jetbrains.intellij ApplePressAndHoldEnabled -bool false
+defaults write com.jetbrains.pycharm ApplePressAndHoldEnabled -bool false
+defaults write com.jetbrains.CLion ApplePressAndHoldEnabled -bool false
