89d2830ebe16c66b681bf68ed6c46e7059758670
Moritz Reiter <moritz@anothernode.com>
2018-03-16 11:16:05 +0100

    Tweak fish key bindings

---
+++ home/.config/fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,8 @@
+# Have magic Ctrl-F completion even in vim keybindings mode
+# https://github.com/fish-shell/fish-shell/issues/3541#issuecomment-260001906
+function fish_user_key_bindings
+    for mode in insert default visual
+        bind -M $mode \cf forward-char
+    end
+end
+
