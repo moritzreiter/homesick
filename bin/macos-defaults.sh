c6094f1e5308bc12324c29b0bcb24035abc57ff3
Moritz Reiter <moritz@anothernode.com>
2018-05-22 16:54:15 +0200

    Disable the scrollbars in macOS Terminal.app

--- bin/macos-defaults.sh
+++ bin/macos-defaults.sh
@@ -3,6 +3,9 @@
 # Display full POSIX path as Finder window title
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 
+# Don't show persistent scrollbars in Terminal app
+defaults write com.apple.Terminal AppleShowScrollBars -string WhenScrolling
+
 # Disable `ApplePressAndHold` in IDEs to avoid conflicts with vim mode
 defaults write com.jetbrains.intellij ApplePressAndHoldEnabled -bool false
 defaults write com.jetbrains.pycharm ApplePressAndHoldEnabled -bool false
