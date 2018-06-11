ff1a89d4f316c5b8d7b2e2ab1f983eac6a208d09
Moritz Reiter <moritz@anothernode.com>
2018-06-11 16:52:17 +0200

    Disable `ApplePressAndHold` for some more tools

--- bin/macos-defaults.sh
+++ bin/macos-defaults.sh
@@ -6,7 +6,12 @@ defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 # Don't show persistent scrollbars in Terminal app
 defaults write com.apple.Terminal AppleShowScrollBars -string WhenScrolling
 
-# Disable `ApplePressAndHold` in IDEs to avoid conflicts with vim mode
+# Disable `ApplePressAndHold` in some development tools
+# InteliJ etc.
 defaults write com.jetbrains.intellij ApplePressAndHoldEnabled -bool false
 defaults write com.jetbrains.pycharm ApplePressAndHoldEnabled -bool false
 defaults write com.jetbrains.CLion ApplePressAndHoldEnabled -bool false
+# VS Code
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+# Boostnone
+defaults write com.maisin.boost ApplePressAndHoldEnabled -bool false
