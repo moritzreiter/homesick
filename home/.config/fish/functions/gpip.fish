e6db550edb0c6b67f08eccdf4533e534e53ffaa6
Moritz Reiter <moritz@anothernode.com>
2018-07-09 14:18:59 +0200

    Clean up fish pip functions

--- home/.config/fish/functions/gpip.fish
+++
@@ -1,6 +0,0 @@
-function gpip3
-  set PIP_REQUIRE_VIRTUALENV false
-  pip3 $argv
-  set PIP_REQUIRE_VIRTUALENV true
-end
-
--- home/.config/fish/functions/gpip3.fish
+++ home/.config/fish/functions/gpip3.fish
@@ -1,1 +1,6 @@
-/Users/moritz/.homesick/repos/dotfiles/home/.config/fish/functions/gpip.fish
+function gpip3
+  set PIP_REQUIRE_VIRTUALENV false
+  pip3 $argv
+  set PIP_REQUIRE_VIRTUALENV true
+end
+
