43f77f51afec9be430dbb3bed813171afd05a3e0
Moritz Reiter <moritz@anothernode.com>
2018-09-10 11:30:12 +0200

    Adjust macOS ApplePressAndHoldEnabled for Boostnote

--- bin/macos-defaults.sh
+++ bin/macos-defaults.sh
@@ -7,11 +7,11 @@ defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 defaults write com.apple.Terminal AppleShowScrollBars -string WhenScrolling
 
 # Disable `ApplePressAndHold` in some development tools
-# InteliJ etc.
+# JetBrains IDEs
 defaults write com.jetbrains.intellij ApplePressAndHoldEnabled -bool false
 defaults write com.jetbrains.pycharm ApplePressAndHoldEnabled -bool false
 defaults write com.jetbrains.CLion ApplePressAndHoldEnabled -bool false
 # VS Code
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
-# Boostnone
-defaults write com.maisin.boost ApplePressAndHoldEnabled -bool false
+# Boostnote
+defaults write com.electron.boostnote ApplePressAndHoldEnabled -bool false
