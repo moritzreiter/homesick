cf41c5e7b41cca9f6f8ec3e95a6b82da00e9318f
Moritz Reiter <moritz@anothernode.com>
2018-10-22 14:19:01 +0200

    Update Java config from Java 9 to 11

--- home/.config/fish/config.macos.fish
+++ home/.config/fish/config.macos.fish
@@ -1,17 +1,17 @@
 # Java
 set -x JAVA_8_HOME ''
-set -x JAVA_9_HOME ''
+set -x JAVA_11_HOME ''
 set java_home_cmd '/usr/libexec/java_home 2>/dev/null --failfast'
 if test (eval $java_home_cmd -v1.8)
     set -x JAVA_8_HOME (eval $java_home_cmd -v1.8)
 end
-if test (eval $java_home_cmd -v9)
-    set -x JAVA_9_HOME (eval $java_home_cmd -v9)
+if test (eval $java_home_cmd -v11)
+    set -x JAVA_11_HOME (eval $java_home_cmd -v11)
 end
 set -x JAVA_HOME $JAVA_8_HOME
 
 alias java8='set -x JAVA_HOME $JAVA_8_HOME'
-alias java9='set -x JAVA_HOME $JAVA_9_HOME'
+alias java11='set -x JAVA_HOME $JAVA_11_HOME'
 
 # Python
 alias pip='pip3'
