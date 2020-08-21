e582956557f06e59d15cab4018ea9c075aa5da89
Moritz Reiter <moritz@anothernode.com>
2020-08-21 10:43:09 +0200

    Add macOS setting to not substitute dot for spaces

--- bin/macos-defaults.sh
+++ bin/macos-defaults.sh
@@ -6,6 +6,9 @@ defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 # Don't show persistent scrollbars in Terminal app
 defaults write com.apple.Terminal AppleShowScrollBars -string WhenScrolling
 
+# Don't substitute dot for two spaces (wtf Apple, how is this on by default?!)
+defaults write -g NSAutomaticPeriodSubstitutionEnabled -bool false
+
 # Disable `ApplePressAndHold` in some development tools
 # JetBrains IDEs
 defaults write com.jetbrains.intellij ApplePressAndHoldEnabled -bool false
@@ -13,5 +16,3 @@ defaults write com.jetbrains.pycharm ApplePressAndHoldEnabled -bool false
 defaults write com.jetbrains.CLion ApplePressAndHoldEnabled -bool false
 # VS Code
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
-# Boostnote
-defaults write com.electron.boostnote ApplePressAndHoldEnabled -bool false
