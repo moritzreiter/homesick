8ab257b30acadf7319076f056a669729a8be4f9e
Moritz Reiter <moritz@anothernode.com>
2021-06-08 20:59:04 +0200

    Improve dry-run check for syncdir script

--- home/.config/fish/functions/syncdir.fish
+++ home/.config/fish/functions/syncdir.fish
@@ -1,8 +1,10 @@
 function syncdir
-  set prompt "DANGER, DANGER! Did you do a --dry-run first (y/n)? "
-  read --local --prompt-str $prompt input
-  if [ $input != y ]
-    return 1
+  if ! contains -- --dry-run $argv
+    set prompt " DANGER, DANGER! Did you do a --dry-run first (y/n)? "
+    read --local --prompt-str $prompt input
+    if [ $input != y ]
+      return 1
+    end
   end
   rsync -avh --delete $argv
 end
