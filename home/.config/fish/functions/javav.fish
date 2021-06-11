cdde480ff60be5565efefbd3d17bdd61313f6b6b
Moritz Reiter <moritz@anothernode.com>
2021-06-11 06:48:24 +0200

    Tweak javav output

--- home/.config/fish/functions/javav.fish
+++ home/.config/fish/functions/javav.fish
@@ -7,10 +7,11 @@ function javav
   for current_version in (seq 8 99)
     set path_to_remove (eval $java_home_cmd $current_version)
     if ! test -z $path_to_remove
+      echo 'Removing' $path_to_remove 'from PATH'
       set PATH (string match --invert $path_to_remove/bin $PATH)
-      echo $PATH
     end
   end
+  echo 'Setting up env for Java' $argv
   set -x JAVA_HOME (eval $java_home_cmd $argv)
   set PATH $JAVA_HOME/bin $PATH
 end
